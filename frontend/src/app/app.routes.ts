df9a427c3e5e1de1a32d5c7379771677b11cc38e
Dee-Olulo <dianaolulo0@gmail.com>
2026-01-06 19:09:17 +0300

    inital home page and login/register page setup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Register } from './pages/register/register';
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login }
+];
